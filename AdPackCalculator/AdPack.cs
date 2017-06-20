da3a61f393688b88861bd2c142eb5646d26e4fc1
Jon Stødle <jonstodle@outlook.com>
2017-06-20 11:53:14 +0200

    Add AdPack model

---
+++ AdPackCalculator/AdPack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdPackCalculator
+{
+    public class AdPack
+    {
+        public DateTimeOffset BuyDate { get; set; }
+        public int Tickets { get; set; } = 120;
+
+        public void TakeTicket() => Tickets -= 1;
+    }
+}
