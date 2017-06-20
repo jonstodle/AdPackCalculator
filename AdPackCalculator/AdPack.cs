c8fed83995d46ab23fd6540f898b77774734b42b
Jon Stødle <jonstodle@outlook.com>
2017-06-20 12:02:19 +0200

    Revert "Add INPC support"
    This reverts commit 95cb61614151e312a34a06250137374e62b6fa74.

--- AdPackCalculator/AdPack.cs
+++ AdPackCalculator/AdPack.cs
@@ -1,32 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdPackCalculator
 {
-    public class AdPack : INotifyPropertyChanged
+    public class AdPack
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public DateTimeOffset BuyDate { get => _buyDate; set => RaisePropertyChanged(ref _buyDate, value); }
-        public int Tickets { get => _tickets; set => RaisePropertyChanged(ref _tickets, value); }
+        public DateTimeOffset BuyDate { get; set; }
+        public int Tickets { get; set; } = 120;
 
         public void TakeTicket() => Tickets -= 1;
-
-        private void RaisePropertyChanged<T>(ref T backingField, T newValue, [CallerMemberName] string propertyName = null)
-        {
-            if (!newValue.Equals(backingField))
-            {
-                backingField = newValue;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-
-        private DateTimeOffset _buyDate;
-        private int _tickets = 120;
     }
 }
