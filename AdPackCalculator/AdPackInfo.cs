0f62f8909e3e90ef5d63f3f956719d046588b059
Jon Stødle <jonstodle@outlook.com>
2017-06-20 12:47:27 +0200

    Add AdPackInfo

---
+++ AdPackCalculator/AdPackInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdPackCalculator
+{
+    public class AdPackInfo
+    {
+        public DateTimeOffset BuyDate { get; set; }
+        public int Amount { get; set; }
+    }
+}
