95cb61614151e312a34a06250137374e62b6fa74
Jon Stødle <jonstodle@outlook.com>
2017-06-20 12:02:19 +0200

    Add INPC support

--- AdPackCalculator/AdPack.cs
+++ AdPackCalculator/AdPack.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdPackCalculator
 {
-    public class AdPack
+    public class AdPack : INotifyPropertyChanged
     {
-        public DateTimeOffset BuyDate { get; set; }
-        public int Tickets { get; set; } = 120;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DateTimeOffset BuyDate { get => _buyDate; set => RaisePropertyChanged(ref _buyDate, value); }
+        public int Tickets { get => _tickets; set => RaisePropertyChanged(ref _tickets, value); }
 
         public void TakeTicket() => Tickets -= 1;
+
+        private void RaisePropertyChanged<T>(ref T backingField, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!newValue.Equals(backingField))
+            {
+                backingField = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private DateTimeOffset _buyDate;
+        private int _tickets = 120;
     }
 }
